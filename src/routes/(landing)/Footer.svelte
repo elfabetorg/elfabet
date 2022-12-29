75c7d9f937f0aebc107266f29f9462d1b97b1f34
Ariel <ariel.kerry@gmail.com>
2022-12-29 22:45:10 +0800

    add footer

--- src/routes/(landing)/Footer.svelte
+++ src/routes/(landing)/Footer.svelte
@@ -1,3 +1,41 @@
 <nav>
-	This is the footer.
+	<div class="links">
+		© 2022 Elfabet |
+		<a href="/legal/privacy-policy">Privacy Policy</a> |
+		<a href="/legal/terms-and-conditions">Terms & Conditions</a>
+	</div>
+	<div class="social-links">
+		<a href="">
+			<img src="footer/footer_email.svg" />
+		</a>
+		<a href="">
+			<img src="footer/footer_discord.svg" />
+		</a>
+		<a href="">
+			<img src="footer/footer_twitter.svg" />
+		</a>
+		<a href="">
+			<img src="footer/footer_instagram.svg" />
+		</a>
+	</div>
 </nav>
+
+<style>
+	nav {
+		margin-top: 72px;
+		color: var(--accent);
+		display: flex;
+		align-content: center;
+		justify-content: center;
+		gap: 88px;
+		padding: 32px;
+		font-size: 14px
+	}
+	a {
+		color: var(--accent);
+	}
+	.social-links {
+		display: flex;
+		gap: 32px;
+	}
+</style>
